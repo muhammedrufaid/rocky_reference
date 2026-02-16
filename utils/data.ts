1872ae0f90aee0d5a75ab4586cc5c52227a4ce70
muhammedrufaid <mohdrufaid7@gmail.com>
2026-02-16 10:43:11 +0400

    first commit

---
+++ utils/data.ts
@@ -0,0 +1,10 @@
+export const navigationData = [
+  { id: 1, title: "Home", path: "/" },
+  { id: 2, title: "About", path: "/about" },
+  { id: 3, title: "Services", path: "/services" },
+  { id: 4, title: "Packages", path: "/packages" },
+  // { id: 5, title: "Blogs", path: "/blogs" },
+  { id: 5, title: "Contact", path: "/contact" },
+];
+
+
