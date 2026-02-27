1fcb5650e7e0f207a4985b17dbfeab5c7c027ef9
muhammedrufaid <mohdrufaid7@gmail.com>
2026-02-27 16:55:38 +0400

    getPropertyTypes APIs

--- utils/getServices.ts
+++ utils/getServices.ts
@@ -17,3 +17,12 @@ export async function getReadyProperties(): Promise<any | undefined> {
       // throw notFound();
     }
 }
+
+export async function getPropertyTypes(): Promise<any | undefined> {
+    try {
+      return await getData(`frontend/properties/types?page=1&limit=20`, 0)
+    } catch (error) {
+      console.error('Failed to fetch featured data:', error)
+      // throw notFound();
+    }
+}
