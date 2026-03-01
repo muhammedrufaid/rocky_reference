9487c0bf22851b23a07afbd84869b3e6cddb3d12
muhammedrufaid <mohdrufaid7@gmail.com>
2026-03-01 18:54:57 +0400

    sidebar updated

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -235,7 +235,7 @@ const Header: React.FC<HeaderProps> = ({ forceSolid = false, hideOnScroll = fals
 
       {/* Sidebar panel */}
       <aside
-        className={`lg:hidden fixed top-0 right-0 bottom-0 z-50 w-[min(320px,85vw)] flex flex-col bg-white shadow-2xl transition-transform duration-300 ease-[cubic-bezier(0.32,0.72,0,1)] ${
+        className={`lg:hidden fixed top-0 right-0 bottom-0 z-50 w-full flex flex-col bg-white shadow-2xl transition-transform duration-300 ease-[cubic-bezier(0.32,0.72,0,1)] ${
           mobileMenuOpen ? "translate-x-0" : "translate-x-full"
         }`}
         aria-label="Mobile navigation"
@@ -248,7 +248,7 @@ const Header: React.FC<HeaderProps> = ({ forceSolid = false, hideOnScroll = fals
               alt="Rocky Real Estate"
               width={110}
               height={44}
-              className="h-8 w-auto"
+              className="h-10 w-auto md:h-12"
             />
           </Link>
           <button
