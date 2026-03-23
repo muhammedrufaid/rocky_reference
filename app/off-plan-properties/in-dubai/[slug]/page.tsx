47860c9fd27b04b677131cabfe155a1864e4c98a
muhammedrufaid <mohdrufaid7@gmail.com>
2026-03-23 17:16:05 +0400

    offplan inidivudal page design start

---
+++ app/off-plan-properties/in-dubai/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import PageHero from "@/components/common/PageHero";
+import TestimonialSection from "@/components/home/TestimonialSection";
+import ServiceSection from "@/components/home/ServiceSection";
+import TeamsIntroSection from "@/components/teams/TeamsntroSection";
+import TeamMembersSection from "@/components/teams/TeamMembersSection";
+import ValuationCTA from "@/components/home/ValuationCTA";
+import WhychooseSection from "@/components/services/WhychooseSection";
+import DevelopmentPartnersSection from "@/components/home/DevelopmentPartnersSection";
+import FaqsSection from "@/components/common/FaqsSection";
+import WhychooseAgentsSection from "@/components/teams/WhychooseAgentsSection";
+import OffPlanIndividualHero from "@/components/properties/OffPlanIndividualHero";
+
+
+export const metadata = {
+    title: "Our Team | Rocky Real Estate",
+    description:
+        "Meet our expert real estate advisors. Browse our team of specialists in sales, leasing, off-plan investments, and property management.",
+};
+
+export default function OffPlanPropertyPage() {
+    return (
+        <div className="min-h-screen bg-white">
+            <Header forceSolid />
+            <main>
+                <OffPlanIndividualHero />
+            </main>
+            <Footer />
+        </div>
+    );
+}
---
+++ components/properties/OffPlanIndividualHero.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Container from '../layout/Container'
+
+type Props = {}
+
+const OffPlanIndividualHero = (props: Props) => {
+  return (
+    <section className="py-10 md:py-12 lg:py-14" aria-label="Property description">
+      <Container>
+        <div>
+          <h1>
+            off plan individual hero title
+          </h1>
+          <h1>
+            off plan individual hero title
+          </h1>
+          <h1>
+            off plan individual hero title
+          </h1>
+          <h1>
+            off plan individual hero title
+          </h1>
+          <h1>
+            off plan individual hero title
+          </h1>
+        </div>
+      </Container>
+    </section>
+  )
+}
+
+export default OffPlanIndividualHero
