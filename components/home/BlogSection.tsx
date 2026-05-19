52af0aca22597df2aa1dbea5a9d5478070bd4a56
muhammedrufaid <mohdrufaid7@gmail.com>
2026-05-19 09:38:07 +0400

    blog list 3 updated

--- components/home/BlogSection.tsx
+++ components/home/BlogSection.tsx
@@ -72,7 +72,7 @@ const BlogSection: React.FC = () => {
           </motion.div>
         </header>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
           {blogPosts.map((post, index) => (
             <motion.article
               key={post.id}
